ff016d49cdbb9b33fe537efb339ba4d913da60ae
Dani Patko <p8.pada@gmail.com>
2023-02-25 11:21:03 +0100

    fix iskolavalasztas

--- iskolavalasztas/iskolavalasztas.cpp
+++ iskolavalasztas/iskolavalasztas.cpp
@@ -5,46 +5,43 @@ using namespace std;
 
 int main()
 {
-    int N, M, temp;
+    int N, M;
     cin >> N >> M;
-    vector<vector<int>> students(N, vector<int>(1));
-    vector<int> schools(M), caps(M), choices(N, -1);
+    vector<vector<int>> students(N, vector<int>(2, 0));
+    vector<int> caps(M), choices(N, -1);
 
     for (int i = 0; i < N; i++)
-    {
-        cin >> students[i][0];
-        cin >> temp;
-        if (temp) 
-            students[i].push_back(temp);
-    }
+        cin >> students[i][0] >> students[i][1];
     for (int i = 0; i < M; i++)
-        cin >> schools[i];
-
-    // put down all students without exceeding capacity
+        cin >> caps[i];
+    
     int i = 0;
     while (i >= 0 && i < N)
     {
-        bool ok = false;
-        for (int j = choices[i] + 1; j < students[i].size(); j++)
+        choices[i]++;
+        if (choices[i] < 2 && 0 < students[i][choices[i]] && 0 < caps[students[i][choices[i]] - 1])
         {
-            if (caps[students[i][j] - 1] + 1 <= schools[students[i][j] - 1])
-            {
-                caps[students[i][j] - 1]++;
-                choices[i++] = j;
-                ok = true;
-                break;
-            }
+            --caps[students[i][choices[i]] - 1];
+            i++;
         }
-
-        if (!ok)
+        else
         {
-            choices[i--] = -1;
-            caps[students[i][choices[i]] - 1]--;
+            choices[i] = -1;
+            i--;
+            if (i == -1) break;
+            ++caps[students[i][choices[i]] - 1];
         }
     }
 
-    for (int j = 0; j < choices.size(); j++)
-        cout << students[j][choices[j]] << " ";
-    cout << "\n";
+    if (i == -1)
+    {
+        cout << "Not possible";
+    }
+    else
+    {
+        for (int j = 0; j < choices.size(); j++)
+            cout << students[j][choices[j]] << " ";
+        cout << "\n";
+    }
 }
 
--- teheneszetek/teheneszetek.cpp
+++ teheneszetek/teheneszetek.cpp
@@ -1,61 +1,14 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 
 using namespace std;
-
-// use the index to get the following: https://cdn.discordapp.com/attachments/1044165803341254676/1061933206946979850/image.png
-void get_vector(int index, vector<int> *v, int M)
-{
-    // last element is index % M
-    // before: (index / M) % M
-    // before before: (index / M^2) % M etc..
-    int div = 1;
-    for (int i = v->size() - 1; i >= 0; i--)
-    {
-        (*v)[i] = (index / div) % M; // note: divisions might be a performance issue
-        div *= M;
-    }
-}
-
-// check the price
-// returns negative uzem index if case is not possible due to capacity
-int calc(vector<int> *current, vector<int> *teheneszetek, vector<int> *uzemek, vector<vector<int>> *prices)
-{
-    int sum = 0, uzem_index;
-    vector<int> capacities(uzemek->size(), 0);
-
-    for (int i = 0; i < current->size(); i++)
-    {
-        uzem_index = (*current)[i];
-        // at full capacity -> not possible, skip
-        capacities[uzem_index] += (*teheneszetek)[i];
-        if (capacities[uzem_index] > (*uzemek)[uzem_index])
-            return -i;
-        // add price to sum
-        sum += (*teheneszetek)[i] * (*prices)[i][uzem_index];
-    }
-
-    return sum;
-}
-
-int skip(vector<int> *current, int from, int N, int M)
-{
-    if (from == N - 1)
-        return 0;
-    int sum = 0, curr = 0;
-    while (curr <= from)
-    {
-        sum += (*current)[curr] * pow(M, N - curr - 1);
-        curr++;
-    }
-    return sum + pow(M, N - from - 1);
-}
 
 int main()
 {
     int N, M;
     cin >> N >> M;
 
-    vector<int> teheneszetek(N), uzemek(M);
+    vector<int> teheneszetek(N), uzemek(M), choices(N, -1), caps(M, 0);
     vector<vector<int>> prices(N, vector<int>(M));
 
     for (int i = 0; i < N; i++)
@@ -66,35 +19,30 @@ int main()
         for (int j = 0; j < M; j++)
             cin >> prices[i][j];
 
-    // time complexity is O(N^M)
-    // instead of using recursion, we can run a single loop,
-    // and determine the current position in each iteration
-    vector<int> current(N), best(N);
-    int best_price = (1 << 30), price, leap;
-
-    for (int i = 0; i < pow(N, M); i++)
+    int i = 0;
+    while (i >= 0)
     {
-        get_vector(i, &current, M);
-        price = calc(&current, &teheneszetek, &uzemek, &prices);
-
-        // if we find an impossible solution, we can just increment
-        // i to avoid unneccessary computation
-        if (price <= 0)
+        if (i >= N)
         {
-            leap = skip(&current, -price, N, M);
-            i = leap == 0 ? i : leap;
-            continue;
+            for (auto& it : choices)
+                cout << it << " ";
+            cout << "\n";
+            i--;
         }
 
-        if (price < best_price)
+        int j = choices[i] + 1;
+        while (j < M && caps[j] + teheneszetek[i] > uzemek[j])
+            j++;
+
+        if (j < M)
         {
-            best_price = price;
-            best = current;
+            caps[j] += teheneszetek[i];
+            choices[i++] = j;
+        }
+        else
+        {
+            caps[choices[i]] -= teheneszetek[i];
+            choices[i--] = -1;
         }
     }
-
-    cout << best_price << "\n";
-    for (int item : best)
-        cout << item + 1 << " ";
-    cout << "\n";
 }
